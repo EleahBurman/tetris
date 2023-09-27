82f60184e66f106f9b66943c07ce2b37238919e4
eleahburman <eleahjburman@gmail.com>
2023-09-27 13:03:03 -0400

    Create end game functionality

--- tetris.js
+++ tetris.js
@@ -105,6 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
       draw()
       displayShape()
       addScore()
+      gameOver()
     }
   }
 
@@ -195,6 +196,14 @@ document.addEventListener('DOMContentLoaded', () => {
         squares = squaresRemoved.concat(squares)
         squares.forEach(cell => grid.appendChild(cell))
       }
+    }
+  }
+
+  //game over
+  function gameOver(){
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+      scoreDisplay.innerHTML = 'Game Over';
+      clearInterval(timerId);
     }
   }
 })
